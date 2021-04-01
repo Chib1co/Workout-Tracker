fa67e91584d98c7436eb65aa7513f92e367e6df2
Tomomi Inoue <73736986+Chib1co@users.noreply.github.com>
2021-04-02 09:38:55 +1100

    adding html routes

---
+++ routes/html-routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const appo = express()
+
+module.exports = function(app) {
+    app.get("/", (req,res) => {
+        res.sendFile(path.join(_dirname, '../public/index.html'));
+    });
+
+
+    app.get("/exercise", (req, res) => {
+        res.sendFile(path.join(_dirname, '../public/exercise.html'));
+    })
+
+    app.get("/stats", (req, res) => {
+        res.sendFile(path.join(_dirname, '../public/stats.html'));
+    })
+
+}
